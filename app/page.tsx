de7d5c894a8869dc7f179ddf7d5fa1b489ecf2b9
meytizn <zamanianmehdi23desktop@gmail.com>
2025-03-30 16:53:49 +0330

    transaction list created with delete function

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,40 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react";
+import { Transaction } from "./Transaction";
 
 export default function Home() {
+
+const [transactions,settransactions]=useState<Transaction[]>([])  //transaction_list
+const [text,settext]=useState("")
+const [amount,setamount]=useState("")
+
+
+
+function addTransaction(){
+  if (!text || !amount) {
+    return alert("dont leave filed empty ")
+  }
+  else{
+    let newtransaction:Transaction={
+      id:Date.now(),
+      text:text,
+      amount:parseFloat(amount)
+    }
+    settransactions([newtransaction, ...transactions])
+    settext("")
+    setamount("")
+  }
+}
+
+
+
+
+
+function deleteTransaction(id:number){
+  settransactions(transactions.filter((item)=> item.id!=id))
+}
+
   return (
       <>
  
@@ -29,7 +63,7 @@ export default function Home() {
 
               <div className="flex flex-row justify-around items-center content-center">
               <div className=" flex flex-row justify-center w-[90%]">
-                 <input type="text" id="success" className="w-[95%] bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block  p-2.5 dark:bg-gray-700 dark:border-green-500" placeholder="title ... " />
+                 <input type="text" value={text} onChange={(e)=>settext(e.target.value)} id="success" className="w-[95%] bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block  p-2.5 dark:bg-gray-700 dark:border-green-500" placeholder="title ... " />
               </div>
               </div>
 
@@ -41,12 +75,14 @@ export default function Home() {
 
                 <div className="flex flex-row w-[82%]">
 
-              <div className=" flex flex-row justify-arround w-[70%] "> 
-                <input type="text" id="success" className="w-[95%] bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block  p-2.5 dark:bg-gray-700 dark:border-green-500" placeholder="price ... " />
+              <div className=" flex flex-row justify-arround w-[70%] ">
+
+                <input type="text" value={amount} onChange={(e)=>setamount(e.target.value)}  id="success" className="w-[95%] bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block  p-2.5 dark:bg-gray-700 dark:border-green-500" placeholder="price ... " />
+             
               </div>
 
               <div className=" flex flex-row justify-arround my-3 items-center content-center justify-center w-[30%]"> 
-                <button type="button" className="w-[100%] text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 my-2">Green</button>
+                <button type="button" onClick={()=>addTransaction()} className="w-[100%] text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 my-2">pay</button>
               </div>
 
 
@@ -54,13 +90,35 @@ export default function Home() {
               </div>
 
               </div>
-
-
-
-
-
 
             </div>
+
+            
+
+
+
+             
+
+          <div className="absolute bottom-0 left-0 right-0 h-[230px] p-5 bg-indigo-500 ">
+            
+            {transactions.map((item)=>(<div className=" flex flex-row md:w-[50%] md:m-auto justify-around items-center content-center bg-white my-2 rounded-md h-[35px]" key={item.id}>
+             
+              <div>{item.text}</div>
+             
+              <div>$ {item.amount}</div>
+
+
+              <div onClick={()=>deleteTransaction(item.id)}><svg className="w-6 h-6 text-red-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18 17.94 6M18 18 6.06 6"/>
+              </svg>
+              </div>
+               
+              
+              </div>))}
+
+          </div>
+
+
       </>
   );
 }
